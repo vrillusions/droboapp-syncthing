d96a78c8b3547b1a41e9a546f12128050ca86000
Todd Eddy <vr@vrillusions.com>
2019-07-27 15:39:16 -0400

    style: Update indentation

--- app.sh
+++ app.sh
@@ -1,16 +1,16 @@
 
 ### syncthing ###
 _build_syncthing() {
-local VERSION="0.14.16"
-local FOLDER="syncthing-linux-arm-v${VERSION}"
-local FILE="${FOLDER}.tar.gz"
-local URL="https://github.com/syncthing/syncthing/releases/download/v${VERSION}/${FILE}"
+  local VERSION="0.14.16"
+  local FOLDER="syncthing-linux-arm-v${VERSION}"
+  local FILE="${FOLDER}.tar.gz"
+  local URL="https://github.com/syncthing/syncthing/releases/download/v${VERSION}/${FILE}"
 
-_download_tgz "${FILE}" "${URL}" "${FOLDER}"
-pushd "target/${FOLDER}"
-mkdir -vp "${DEST}/app"
-mv -v ./* "${DEST}/app/"
-popd
+  _download_tgz "${FILE}" "${URL}" "${FOLDER}"
+  pushd "target/${FOLDER}"
+  mkdir -vp "${DEST}/app"
+  mv -v ./* "${DEST}/app/"
+  popd
 }
 
 ### BUILD ###
